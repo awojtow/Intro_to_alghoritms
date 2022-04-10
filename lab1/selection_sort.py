d645ce89d60b1ed903d1b2df1f6cbac4afa8190a
amwt <58270970+awojtow@users.noreply.github.com>
2022-04-10 15:55:17 +0200

    fixed comms

--- lab1/selection_sort.py
+++ lab1/selection_sort.py
@@ -4,12 +4,12 @@ from check_time import check_time
 @check_time
 def selection_sort(lst):
     n = len(lst)
-    for i in range(n): #dla kazdego elementu listy
-        idx_min = i #ustal jako minimum dany element 
-        for j in range(i+1,n): #dla wszystkich następnych elementów sprawdz 
-            #czy nie sa przypadkiem mniejsze niz załoone minimum
-            if lst[j]<lst[idx_min]: #porównaj 
-                idx_min = j #jeśli którys z dalszych elemtnów jest mniejszy - ustal nowe minimym
-        lst[i], lst[idx_min] = lst[idx_min], lst[i] #zamien mijscami elementy - minimum oraz dany elemetn tej pętli
+    for i in range(n): 
+        idx_min = i 
+        for j in range(i+1,n): 
+           
+            if lst[j]<lst[idx_min]: 
+                idx_min = j 
+        lst[i], lst[idx_min] = lst[idx_min], lst[i] 
     return lst 
 
